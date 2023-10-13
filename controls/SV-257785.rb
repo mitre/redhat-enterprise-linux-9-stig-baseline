ca024e0c50285573895e8234af7a009c7f6fcc27
jmetzger <Jon.metzger@me.com>
2023-10-13 15:27:18 -0400

    SV-257785

--- controls/SV-257785.rb
+++ controls/SV-257785.rb
@@ -29,4 +29,21 @@ $ sudo systemctl mask --now ctrl-alt-del.target'
   tag 'documentable'
   tag cci: ['CCI-000366', 'CCI-002235']
   tag nist: ['CM-6 b', 'AC-6 (10)']
+
+  if virtualization.system.eql?('docker')
+    impact 0.0
+    describe "Control not applicable within a container" do
+      skip "Control not applicable within a container"
+    end
+  else
+    describe.one do
+      c = systemd_service('ctrl-alt-del.target')
+      describe c do
+        its('params.LoadState') { should eq 'masked' }
+      end
+      describe c do
+        its('params.LoadState') { should eq 'not-found' }
+      end
+    end
+  end
 end
