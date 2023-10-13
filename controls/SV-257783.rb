0a3e5bcde69982fe056967991fa6274ac58d1cfd
jrmetzger <9845417+jrmetzger@users.noreply.github.com>
2023-10-13 19:03:53 -0400

    cookstyle

--- controls/SV-257783.rb
+++ controls/SV-257783.rb
@@ -24,10 +24,10 @@ $ sudo systemctl enable --now systemd-journald'
   tag cci: ['CCI-001665']
   tag nist: ['SC-24']
 
-  describe package("systemd-journal-remote") do
+  describe package('systemd-journal-remote') do
     it { should be_installed }
   end
-  describe service("systemd-journal-upload") do
+  describe service('systemd-journal-upload') do
     it { should be_enabled }
   end
 end
