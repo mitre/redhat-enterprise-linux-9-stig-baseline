d1d65d7100acc7683fdecfd93222b38c7c64527b
jmetzger <Jon.metzger@me.com>
2023-10-27 15:47:10 -0400

    SV-257885

--- controls/SV-257885.rb
+++ controls/SV-257885.rb
@@ -25,4 +25,8 @@ $ sudo chmod 0755 /var/log'
   tag 'documentable'
   tag cci: ['CCI-001314']
   tag nist: ['SI-11 b']
+
+  describe directory('/var/log') do
+    it { should_not be_more_permissive_than('0755') }
+  end
 end
