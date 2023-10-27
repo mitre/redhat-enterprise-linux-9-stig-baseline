7d93274b9a4bcabbc350e9b1fbc1956168e9e130
jmetzger <Jon.metzger@me.com>
2023-10-27 15:16:16 -0400

    SV-257865

--- controls/SV-257865.rb
+++ controls/SV-257865.rb
@@ -21,4 +21,8 @@ If the /dev/shm file system is mounted without the "noexec" option, this is a fi
   tag 'documentable'
   tag cci: ['CCI-001764']
   tag nist: ['CM-7 (2)']
+
+  describe etc_fstab.where { mount_point == '/dev/shm' } do
+    its('mount_options.flatten') { should include 'noexec' }
+  end
 end
