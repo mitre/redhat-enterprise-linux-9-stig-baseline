1a4bc5d39b2daabd4575708d71db1e0be9c15a13
Amndeep Singh Mann <amann@mitre.org>
2024-10-10 16:40:46 -0400

    lint
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- controls/SV-257949.rb
+++ controls/SV-257949.rb
@@ -40,7 +40,7 @@ $ sudo systemctl reload NetworkManager'
       end
     end
   else
-    describe ini({content: network_manager.stdout.strip}) do
+    describe ini({ content: network_manager.stdout.strip }) do
       its('main.dns') { should exist }
       its('main.dns') { should be_in ['none', 'default'] }
     end
