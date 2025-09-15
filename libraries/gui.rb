39f9076bb8d1410e33774fba226d8dd0f511626a
Amndeep Singh Mann <amann@mitre.org>
2025-09-15 18:04:06 -0400

    did not actually include the gui resource at all in the prior commit so here it is now, nor did i include the input i created so added that too, and also fixed the logic of the control to properly handle all logic paths for gnome/non-gnome DEs being installed which ended up being a bit more convulated than I like due to how passes/fails have higher priority than skips in how we calculate the overarching control status
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

---
+++ libraries/gui.rb
@@ -0,0 +1,36 @@
+class Gui < Inspec.resource(1)
+  name 'gui'
+
+  supports platform: 'redhat', release: '9.*'
+
+  def installed_guis()
+    user_specified_packages = inspec.input('possibly_installed_guis')
+    user_specified_installed_packages = user_specified_packages.select do |package|
+      inspec.package(package).installed?
+    end
+
+    desktop_files_to_packages_command = inspec.command('ls -w 1 /usr/share/xsessions/ | xargs -I{} rpm -qf /usr/share/xsessions/{} --qf "%{NAME}\n" | uniq')
+    if desktop_files_to_packages_command.exit_status != 0
+      desktop_packages = desktop_files_to_packages_command.stdout.split("\n")
+    else
+      desktop_packages = []
+    end
+
+    @installed_guis ||= (user_specified_installed_packages + desktop_packages).uniq
+  end
+
+  def installed_non_gnome_guis()
+    @installed_non_gnome_guis ||= installed_guis - installed_guis.all?(/gnome/)
+  end
+
+  def has_gui?()
+    !installed_guis.empty?
+  end
+
+  def has_gnome_gui?()
+    installed_guis.any?(/gnome/)
+  end
+
+  def has_non_gnome_gui?()
+    installed_non_gnome_guis.empty?
+end
