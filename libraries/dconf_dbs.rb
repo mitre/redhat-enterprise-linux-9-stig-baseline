6ec36a6346231c3820ce1140087b01821d4cfeb9
Amndeep Singh Mann <amann@mitre.org>
2025-09-16 02:34:01 -0400

    added support for keyfiles and lockfiles in the resource, got rid of the notes, and have a first attempt at testing against the keyfile mtimes
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- libraries/dconf_dbs.rb
+++ libraries/dconf_dbs.rb
@@ -11,9 +11,12 @@ class DConfDBs < Inspec.resource(1)
   ft.register_column(:mtime, field: :mtime)
   ft.register_column(:keyfile_dir, field: :keyfile_dir)
   ft.register_column(:keyfile_dir_exists, field: :keyfile_dir_exists)
+  ft.register_column(:keyfiles, field: :keyfiles)
+  ft.register_column(:lockfiles, field: :lockfiles)
 
   ft.register_custom_matcher(:has_keyfiles_dir?) { |table| table.keyfile_dir_exists.all? }
-  # matcher for the mtime comparison
+  ft.register_custom_matcher(:has_latest_keyfiles_dir_updates?) { |table| table.where { keyfiles.any? { |kf| mtime < kf.mtime } }.empty? }
+  ft.register_custom_matcher(:has_latest_lockfiles_dir_updates?) { |table| table.where { lockfiles.any? { |lf| mtime < lf.mtime } }.empty? }
 
   ft.install_filter_methods_on_resource(self, :collect_dconf_dbs_details)
 
@@ -24,7 +27,19 @@ class DConfDBs < Inspec.resource(1)
   private
 
   def collect_dconf_dbs_details
-    dbs = inspec.command('find /etc/dconf/db -maxdepth 1 -type f').stdout.strip.split("\n")
-    dbs.map { |db| { name: db, mtime: inspec.file(db).mtime, keyfile_dir: "#{db}.d/", keyfile_dir_exists:  inspec.file("#{db}.d/").exist? } } # add in keyfiles + lock files
+    dbs = inspec.command('find /etc/dconf/db/ -maxdepth 1 -type f').stdout.strip.split("\n")
+    dbs.map { |db|
+      keyfile_dir_exists = inspec.file("#{db}.d/").exist?
+      keyfiles = keyfile_dir_exists ? inspec.command("find /etc/dconf/db/#{db}.d/ -maxdepth 1 -type f").stdout.strip.split("\n").map { |f| { name: f, mtime: inspec.file(f).mtime } } : []
+      lockfiles = keyfile_dir_exists && inspec.file("#{db}.d/locks").exist? ? inspec.command("find /etc/dconf/db/#{db}.d/locks -maxdepth 1 -type f").stdout.strip.split("\n").map { |f| { name: f, mtime: inspec.file(f).mtime } } : []
+      {
+        name: db,
+        mtime: inspec.file(db).mtime,
+        keyfile_dir: "#{db}.d/",
+        keyfile_dir_exists: keyfile_dir_exists
+        keyfiles: keyfiles
+        lockfiles: lockfiles
+      }
+    }
   end
 end
