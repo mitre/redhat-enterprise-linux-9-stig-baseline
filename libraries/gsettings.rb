543ba1a876a85b835d559a259fecac55466f9e30
Amndeep Singh Mann <amann@mitre.org>
2025-09-16 17:14:43 -0400

    first pass at gsettings resource and test using it
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

---
+++ libraries/gsettings.rb
@@ -0,0 +1,36 @@
+class Gsettings < Inspec.resource(1)
+  name 'gsettings'
+
+  supports platform: 'redhat', release: '9.*'
+
+  def initialize(key, schema, schemadir = nil)
+    @key = key
+    @schema = schema
+    @schemadir = schemadir
+    @schemadir_snippet = schemadir == nil ? '' : " --schemadir #{@schemadir}"
+  end
+
+  def get()
+    @get ||= inspec.command("gsettings#{@schemadir_snippet} get #{schema} #{key}")
+  end
+
+  def writable()
+    @writable ||= inspec.command("gsettings#{@schemadir_snippet} writable #{schema} #{key}")
+  end
+
+  def exist?()
+    @get.stderr.strip.empty?
+  end
+
+  def true?()
+    @get.stdout.strip == "true"
+  end
+
+  def locked?()
+    @writable.stdout.strip == "false"
+  end
+
+  def to_s
+    "gsettings#{@schemadir == nil ? '' : "(#{@schemadir})"} #{@schema} #{@key}"
+  end
+end
