10fac922f079e33ffef48b53d6bc83efbe09d6fa
Amndeep Singh Mann <amann@mitre.org>
2025-09-17 23:58:47 -0400

    it's getting late and i'm not feeling the best which is why i completely lost the thread on why i had to set up the relatively convoluted (but less than before!) logic for the gui checks.  hopefully i'll remember to post pictures of my notebook tomorrow where i have it written down as to what's happening but basically in this case we can't allow the pass to go through in the has_non_gnome_gui branch because it'll clobber the skip->manual review that i'm trying to set up.  the fail clobbering it is fine cause a fail's a fail, but we can't let the pass do it otherwise it'll actually be a false/incomplete positive since you still need to check the other DEs to confirm that they also meet the requirement.
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- controls/SV-258018.rb
+++ controls/SV-258018.rb
@@ -42,13 +42,33 @@ AutomaticLoginEnable=false'
       skip 'A GUI/desktop environment is not installed; this control is Not Applicable.'
     end
   else
-    if g.has_gnome_gui?
-      conf_path = '/etc/gdm/custom.conf'
-      conf_file = file(conf_path)
+    conf_path = '/etc/gdm/custom.conf'
+    conf_file = file(conf_path)
+    conf = parse_config_file(conf_path)
 
+    if g.has_non_gnome_gui?
+      if g.has_gnome_gui?
+        if !conf_file.exist?
+          describe conf_file do
+            it { should exist }
+          end
+        elsif conf.params['daemon']['AutomaticLoginEnable'] == 'false'
+          describe "`gdm` config file at #{conf_path}" do
+            subject { conf }
+            it 'should have `AutomaticLoginEnable` set to `false` in the `[daemon]` section.' do
+              expect(subject.params['daemon']['AutomaticLoginEnable']).to eq ('false')
+            end
+          end
+        end
+      end
+
+      describe 'Non-GNOME desktop environments detected' do
+        skip "Manual check required as there is no guidance for non-GNOME desktop environments, which were identified as being installed on the system.  Investigate the following, possibly related packages to determine which desktop environments are installed and then determine a method to ensure that each of those desktop environments' configuration is up-to-date and matches policy:\n\t- #{g.installed_non_gnome_guis.join("\n\t- ")}"
+      end
+    else
       if conf_file.exist?
         describe "`gdm` config file at #{conf_path}" do
-          subject { parse_config_file(conf_path) }
+          subject { conf }
           it 'should have `AutomaticLoginEnable` set to `false` in the `[daemon]` section.' do
             expect(subject.params['daemon']['AutomaticLoginEnable']).to eq ('false')
           end
@@ -57,12 +77,6 @@ AutomaticLoginEnable=false'
         describe conf_file do
           it { should exist }
         end
-      end
-    end
-
-    if g.has_non_gnome_gui?
-      describe 'Non-GNOME desktop environments detected' do
-        skip "Manual check required as there is no guidance for non-GNOME desktop environments, which were identified as being installed on the system.  Investigate the following, possibly related packages to determine which desktop environments are installed and then determine a method to ensure that each of those desktop environments' configuration is up-to-date and matches policy:\n\t- #{g.installed_non_gnome_guis.join("\n\t- ")}"
       end
     end
   end
