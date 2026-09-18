909ef796ec87eec4f9e25c1abb5eb7f539bc411c
Princeton Wong <pwong@mitre.org>
2026-09-18 11:33:23 -0700

    virtualization update remaining conditions

--- controls/SV-257879.rb
+++ controls/SV-257879.rb
@@ -71,7 +71,7 @@ To encrypt an entire partition, dedicate a partition for encryption in the parti
 
   # TODO: This should really have a resource
 
-  if %w[docker podman kubepods lxc].include?(virtualization.system)
+  if virtualization.container_system?
     impact 0.0
     describe_and_skip('Disk Encryption and Data At Rest Implementation is handled on the Container Host')
   elsif input('data_at_rest_exempt')
